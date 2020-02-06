24d73a52ca92b62bda7a85cc0650f103a0e36a63
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-06 17:36:39 +0000

    msmnile-user 9 PKQ1.190821.001 219 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ rootdir/bin/init.vendor.sh
@@ -0,0 +1,8 @@
+#! /vendor/bin/sh
+
+if [ -d "/data/vendor/tombstones/SDX50M" ]; then
+    chmod -R 775 /data/vendor/tombstones/SDX50M
+fi
+if [ -d "/data/vendor/tombstones/SDX55M" ]; then
+    chmod -R 775 /data/vendor/tombstones/SDX55M
+fi
